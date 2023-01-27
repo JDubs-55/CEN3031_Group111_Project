35ddded717e17c6a10c7af88796d8bbaf30aa1fd
buildmine10 <33100363+buildmine10@users.noreply.github.com>
2023-01-26 21:16:38 -0500

    Added Flash Card component and service

--- FrontEnd/Squizzy/src/app/app.component.ts
+++ FrontEnd/Squizzy/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FlashCardControllerService } from './flash-card-controller.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Squizzy';
+  displayedDeck = "deck1";
+  cardNumber = 0;
+
+  constructor(private flashCardController: FlashCardControllerService){
+    this.flashCardController.addDeck(this.displayedDeck);
+    this.flashCardController.addCardFromTemplate(this.displayedDeck, "default", "Test Front Text", "Test Back Text");
+  }
 }
