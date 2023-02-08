e30f56532b2c9f37726c12bdd968c49d20723252
buildmine10 <33100363+buildmine10@users.noreply.github.com>
2023-02-08 11:40:25 -0500

    Published Second Draft

--- FrontEnd/Squizzy/src/app/app.component.ts
+++ FrontEnd/Squizzy/src/app/app.component.ts
@@ -7,12 +7,8 @@ import { FlashCardControllerService } from './flash-card-controller.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Squizzy';
-  displayedDeck = "deck1";
-  cardNumber = 0;
 
-  constructor(private flashCardController: FlashCardControllerService){
-    this.flashCardController.addDeck(this.displayedDeck);
-    this.flashCardController.addCardFromTemplate(this.displayedDeck, "default", "Test Front Text", "Test Back Text");
+  constructor(){
+
   }
 }
