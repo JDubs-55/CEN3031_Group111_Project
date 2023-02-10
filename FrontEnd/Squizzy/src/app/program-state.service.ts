bd3e94c41d9d477ef38d843b45f21e714277589f
buildmine10 <33100363+buildmine10@users.noreply.github.com>
2023-02-10 15:02:48 -0500

    Added Funtionality to Program State

--- FrontEnd/Squizzy/src/app/program-state.service.ts
+++ FrontEnd/Squizzy/src/app/program-state.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Deck } from './MyClasses/Deck';
+import { CardData } from './MyClasses/CardData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgramStateService {
+  selectedDeck?: Deck;
+
 
   constructor() { }
+
+
+  getCardList(): readonly CardData[]{
+    if(this.selectedDeck == undefined){
+      return [];
+    }
+    return Object.values(this.selectedDeck.cards);
+  }
+
+
 }
