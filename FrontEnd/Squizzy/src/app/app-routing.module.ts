8ae00dc8ee4d3f2145f2fecc41cf505b301fca4f
buildmine10 <33100363+buildmine10@users.noreply.github.com>
2023-02-11 13:50:59 -0500

    Initialized Deck Editor

--- FrontEnd/Squizzy/src/app/app-routing.module.ts
+++ FrontEnd/Squizzy/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeckEditorComponent } from './deck-editor/deck-editor.component';
+import { SelectedDeckDisplayComponent } from './selected-deck-display/selected-deck-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"DeckEditor", component: DeckEditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/Squizzy/src/app/app.component.ts
+++ FrontEnd/Squizzy/src/app/app.component.ts
@@ -15,65 +15,10 @@ import { CardData } from './MyClasses/CardData';
 export class AppComponent {
 
   constructor(private deckManager: DeckManagerService) {
-    /*
-    for(let i = 0; i < 13; i++){
-      this.generateRandomDeck(5);
-    }
-    */
-  
     
   }
 
   ngOnInit(){
     this.deckManager.loadAllDeckNames();
-  }
-
-  private generateRandomDeck(numberOfCards: number = 1): void {
-    function makeid(length: number): string {
-      let result = '';
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      const charactersLength = characters.length;
-      let counter = 0;
-      while (counter < length) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-      }
-      return result;
-    }
-
-    let deck = this.deckManager.generateDeck();
-    deck.name = "Deck" + Object.keys(this.deckManager.loadedDecks).length + " " + makeid(5);
-
-    for(let i = 0; i < numberOfCards; i++){
-      deck.addCard({
-        ID: makeid(10),
-        isFavorite: false,
-        frontText: "front: " + makeid(10),
-        backText: "back: " + makeid(10),
-      });
-    }
-  }
-
-  changeFrontText(): void{
-    function makeid(length: number): string {
-      let result = '';
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      const charactersLength = characters.length;
-      let counter = 0;
-      while (counter < length) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-      }
-      return result;
-    }
-
-
-
-    Object.values(this.deckManager.searchDecksByName("Genki")[0].cards).forEach(card=>{
-      this.deckManager.searchDecksByName("Genki")[0].editCard({
-        ID: card.ID,
-        frontText: "front: " + makeid(3)
-      })
-    })
   }
 }
